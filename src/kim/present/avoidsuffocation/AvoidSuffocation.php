f5b1331b767ff343a8a1f8e02496c67d8ebd2747
PresentKim <debe3721@gmail.com>
2024-02-10 01:24:35 +0900

    :art: Code formatting with .editorconfig

--- src/kim/present/avoidsuffocation/AvoidSuffocation.php
+++ src/kim/present/avoidsuffocation/AvoidSuffocation.php
@@ -12,9 +12,9 @@
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author  PresentKim (debe3721@gmail.com)
- * @link    https://github.com/PresentKim
- * @license https://www.gnu.org/licenses/lgpl-3.0 LGPL-3.0 License
+ * @author       PresentKim (debe3721@gmail.com)
+ * @link         https://github.com/PresentKim
+ * @license      https://www.gnu.org/licenses/lgpl-3.0 LGPL-3.0 License
  *
  *   (\ /)
  *  ( . .) ♥
@@ -34,40 +34,48 @@ use pocketmine\math\Facing;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 
+use function count;
+use function is_dir;
+use function rmdir;
+use function scandir;
+
 class AvoidSuffocation extends PluginBase implements Listener{
-    public function onEnable() : void{
-        /**
-         * This is a plugin that does not use data folders.
-         * Delete the unnecessary data folder of this plugin for users.
-         */
-        $dataFolder = $this->getDataFolder();
-        if(is_dir($dataFolder) && count(scandir($dataFolder)) <= 2){
-            rmdir($dataFolder);
-        }
+	public function onEnable() : void{
+		/**
+		 * This is a plugin that does not use data folders.
+		 * Delete the unnecessary data folder of this plugin for users.
+		 */
+		$dataFolder = $this->getDataFolder();
+		if(is_dir($dataFolder) && count(scandir($dataFolder)) <= 2){
+			rmdir($dataFolder);
+		}
 
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    }
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
 
-    /** @priority HIGHEST */
-    public function onEntityDamageEvent(EntityDamageEvent $event) : void{
-        if($event->getCause() !== EntityDamageEvent::CAUSE_SUFFOCATION){
-            return;
-        }
+	/** @priority HIGHEST */
+	public function onEntityDamageEvent(EntityDamageEvent $event) : void{
+		if($event->getCause() !== EntityDamageEvent::CAUSE_SUFFOCATION){
+			return;
+		}
 
-        $entity = $event->getEntity();
-        if(!$entity instanceof Player){
-            return;
-        }
+		$entity = $event->getEntity();
+		if(!$entity instanceof Player){
+			return;
+		}
 
-        $world = $entity->getWorld();
-        $pos = $entity->getPosition();
-        foreach(Facing::HORIZONTAL as $face){
-            $blockVec = $pos->getSide($face);
-            if($world->getBlock($blockVec->up())->getTypeId() === BlockTypeIds::AIR && $world->getBlock($blockVec)->getTypeId() === BlockTypeIds::AIR){
-                $entity->setMotion($blockVec->subtractVector($pos)->multiply(0.1));
-                $event->cancel();
-                return;
-            }
-        }
-    }
+		$world = $entity->getWorld();
+		$pos = $entity->getPosition();
+		foreach(Facing::HORIZONTAL as $face){
+			$blockVec = $pos->getSide($face);
+			if(
+				$world->getBlock($blockVec->up())->getTypeId() === BlockTypeIds::AIR
+				&& $world->getBlock($blockVec)->getTypeId() === BlockTypeIds::AIR
+			){
+				$entity->setMotion($blockVec->subtractVector($pos)->multiply(0.1));
+				$event->cancel();
+				return;
+			}
+		}
+	}
 }
